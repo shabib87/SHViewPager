287c660b047227ce103965c507b32d6a0ac937c6
Ahmad Shabibul Hossain <shabib.hossain@iappdragon.com>
2016-08-02 14:01:40 +0600

    documentation updated

--- SHViewPager/SHViewPager.h
+++ SHViewPager/SHViewPager.h
@@ -30,81 +30,104 @@
 
 @class SHViewPager;
 
-// enum to determine topbar menu button width
+/**
+ enum to determine topbar menu button width
+ */
 typedef NS_ENUM(NSInteger, SHViewPagerMenuWidthType) {
-    SHViewPagerMenuWidthTypeDefault, // default width for menus, (screen_width / 5) i.e.64points wide in iPhone
-    SHViewPagerMenuWidthTypeWide, // wider menus, (screen_width / 3) i.e. 107points wide in iPhone
-    SHViewPagerMenuWidthTypeNarrow // narrow menus, (screen_width / 7) i.e. 46points wide in iPhone
+    SHViewPagerMenuWidthTypeDefault, /** default width for menus, (screen_width / 5) i.e.64points wide in iPhone */
+    SHViewPagerMenuWidthTypeWide, /** wider menus, (screen_width / 3) i.e. 107points wide in iPhone */
+    SHViewPagerMenuWidthTypeNarrow /** narrow menus, (screen_width / 7) i.e. 46points wide in iPhone */
 };
 
 @protocol SHViewPagerDataSource <NSObject>
 
 @required
 
-// total number of pages to be displayed by the controller
+/** total number of pages to be displayed by the controller */
 - (NSInteger)numberOfPagesInViewPager:(SHViewPager *)viewPager;
 
-// the viewcontroller that will contain the pages, in most of the cases it will be the same viewcontroller that is acting as the datasource and delegate
-// i.e. return value will be 'self'
+/**
+ the viewcontroller that will contain the pages, in most of the cases it will be the same viewcontroller that is acting as the datasource and delegate;
+ i.e. return value will be 'self'.
+ */
 - (UIViewController *)containerControllerForViewPager:(SHViewPager *)viewPager;
 
-// the viewcontroller that is to be shown as as a page in the pager
+/** the viewcontroller that is to be shown as as a page in the pager */
 - (UIViewController *)viewPager:(SHViewPager *)viewPager controllerForPageAtIndex:(NSInteger)index;
 
 @optional
 
-// the indicator image for the current index page
-// preferred image size is screen_width x 7
-// i.e. if app runs on iphone, image size should be 320x7
+/** 
+ the indicator image for the current index page.
+ preferred image size is screen_width x 7;
+ i.e. if app runs on iphone 5, image size should be 320x7;
+ */
 - (UIImage *)indexIndicatorImageForViewPager:(SHViewPager *)viewPager;
 
-// the indicator image during the scrolling animation state
-// preferred image size is screen_width x 7
-// i.e. if app runs on iphone, image size should be 320x7
-// run and check example code to understand the behavior more clearly
+/**
+ the indicator image during the scrolling animation state.
+ preferred image size is screen_width x 7;
+ i.e. if app runs on iphone 5, image size should be 320x7.
+ run and check example code to understand the behavior more clearly.
+ */
 - (UIImage *)indexIndicatorImageDuringScrollAnimationForViewPager:(SHViewPager *)viewPager;
 
-// Important: either viewPager:imageForPageMenuAtIndex or viewPager:titleForPageMenuAtIndex has to be implemented, but not both
-
-// the title to be displayed on the menu button,
-// don't implement if viewPager:imageForPageMenuAtIndex is already implemented
+/** 
+ Important: either viewPager:imageForPageMenuAtIndex or viewPager:titleForPageMenuAtIndex has to be implemented, but not both.
+ the title to be displayed on the menu button.
+ don't implement if viewPager:imageForPageMenuAtIndex is already implemented.
+ */
 - (NSString *)viewPager:(SHViewPager *)viewPager titleForPageMenuAtIndex:(NSInteger)index;
 
-// the image to be displayed on the menu button
-// don't implement if viewPager:titleForPageMenuAtIndex is already implemented
-// preferred image size is button_width x 44
-// i.e. if menuWidthTypeInViewPager: returns SHViewPageMenuWidthTypeDefault, then
-// image size should be 64x44 in iPhone
+/**
+ the image to be displayed on the menu button.
+ don't implement if viewPager:titleForPageMenuAtIndex is already implemented.
+ preferred image size is button_width x 44;
+ i.e. if menuWidthTypeInViewPager: returns SHViewPageMenuWidthTypeDefault, then
+ image size should be 64x44 in iPhone.
+ */
 - (UIImage *)viewPager:(SHViewPager *)viewPager imageForPageMenuAtIndex:(NSInteger)index;
 
-// the title to be displayed right below the indicator image
-// if not implemented, the area should be hidden and covered by the content view
+/**
+ the title to be displayed right below the indicator image
+ if not implemented, the area should be hidden and covered by the content view
+ */
 - (NSString *)viewPager:(SHViewPager *)viewPager headerTitleForPageMenuAtIndex:(NSInteger)index;
 
-// the image to be displayed for the menu button highlited state
-// don't implement if viewPager:titleForPageMenuAtIndex is already implemented
-// preferred image size is button_width x 44
-// i.e. if menuWidthTypeInViewPager: returns SHViewPageMenuWidthTypeDefault, then
-// image size should be 64x44 in iPhone
+/**
+ the image to be displayed for the menu button highlited state.
+ don't implement if viewPager:titleForPageMenuAtIndex is already implemented.
+ preferred image size is button_width x 44;
+ i.e. if menuWidthTypeInViewPager: returns SHViewPageMenuWidthTypeDefault, then
+ image size should be 64x44 in iPhone.
+ */
 - (UIImage *)viewPager:(SHViewPager *)viewPager highlitedImageForPageMenuAtIndex:(NSInteger)index;
 
-// the image to be displayed for the menu button selected state
-// don't implement if viewPager:titleForPageMenuAtIndex is already implemented
-// preferred image size is button_width x 44
-// i.e. if menuWidthTypeInViewPager: returns SHViewPageMenuWidthTypeDefault, then
-// image size should be 64x44 in iPhone
+/**
+ the image to be displayed for the menu button selected state.
+ don't implement if viewPager:titleForPageMenuAtIndex is already implemented.
+ preferred image size is button_width x 44;
+ i.e. if menuWidthTypeInViewPager: returns SHViewPageMenuWidthTypeDefault, then
+ image size should be 64x44 in iPhone.
+ */
 - (UIImage *)viewPager:(SHViewPager *)viewPager selectedImageForPageMenuAtIndex:(NSInteger)index;
 
-// menu width type for the top bar
-// if not implemented SHViewPagerMenuWidthTypeDefault will be counted
+/**
+ menu width type for the top bar.
+ if not implemented SHViewPagerMenuWidthTypeDefault will be counted.
+ */
 - (SHViewPagerMenuWidthType)menuWidthTypeInViewPager:(SHViewPager *)viewPager;
 
-// background color for the top bar
-// if not implemented [UIColor whiteColor] will be used
+/** 
+ background color for the top bar.
+ if not implemented [UIColor whiteColor] will be used.
+ */
 - (UIColor *)colorForMenuInViewPager:(SHViewPager *)viewPager;
 
-// font for the menu
-// if not implemented, the system font will be used
+/**
+ font for the menu.
+ if not implemented, the system font will be used.
+ */
 - (UIFont *)fontForMenu:(SHViewPager *)viewPager;
 
 @end
@@ -113,13 +136,13 @@ typedef NS_ENUM(NSInteger, SHViewPagerMenuWidthType) {
 
 @optional
 
-// called when page is about to move
+/** called when page is about to move. */
 - (void)viewPager:(SHViewPager *)viewPager willMoveToPageAtIndex:(NSInteger)toIndex fromIndex:(NSInteger)fromIndex;
 
-// called when a page has moved
+/** called when a page has moved. */
 - (void)viewPager:(SHViewPager *)viewPager didMoveToPageAtIndex:(NSInteger)toIndex fromIndex:(NSInteger)fromIndex;
 
-// called when the first page is loaded
+/** called when the first page is loaded. */
 - (void)firstContentPageLoadedForViewPager:(SHViewPager *)viewPager;
 
 @end
@@ -129,23 +152,27 @@ typedef NS_ENUM(NSInteger, SHViewPagerMenuWidthType) {
 @property (nonatomic, weak) IBOutlet id <SHViewPagerDataSource> dataSource;
 @property (nonatomic, weak) IBOutlet id <SHViewPagerDelegate> delegate;
 
-// returns the array of the menu buttons
+/** returns the array of the menu buttons. */
 @property (nonatomic, readonly) NSArray *menuButtons;
 
-// returns a dictionay of the content viewcontrollers, key for the value is set as their index
+/** returns a dictionay of the content viewcontrollers, key for the value is set as their index. */
 @property (nonatomic, readonly) NSDictionary *contentViewControllers;
 
-// populates or, re-populates the pager contents
-// reloads everything from scratch
-// pager will display nothing unless this method is called
-// if the datasource is not changing, call it once at the begining
-// if the datasource is dynamic and changing, call this method everytime you need
-// to re-populate the pager (as you do in UITableView or, UIPickerView or, UICollectionView)
+/**
+ populates or, re-populates the pager contents.
+ reloads everything from scratch.
+ pager will display nothing unless this method is called.
+ if the datasource is not changing, call it once at the begining.
+ if the datasource is dynamic and changing, call this method everytime you need.
+ to re-populate the pager (as you do in UITableView or, UIPickerView or, UICollectionView).
+ */
 - (void)reloadData;
 
-// fixes bug for scrollview's content offset reset.
-// this is a hacky solution, any better solution is welcome.
-// check closed issues #1 & #2 for more details.
+/**
+ fixes bug for scrollview's content offset reset.
+ this is a hacky solution, any better solution is welcome.
+ check closed issues #1 & #2 for more details.
+ */
 -(void)pagerWillLayoutSubviews;
 
 @end
