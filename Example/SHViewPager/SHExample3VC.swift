aa053992bf19e82ac982bbdfd779bed484af14cf
shabib87 <shabib.sust@gmail.com>
2016-08-28 23:31:52 +0600

    Example 3 started.

--- Example/SHViewPager/SHExample3VC.swift
+++ Example/SHViewPager/SHExample3VC.swift
@@ -11,28 +11,14 @@ import SHViewPager
 
 class SHExample3VC: UIViewController {
 
-    private var pager: SHViewPager!
+    @IBOutlet var pager: SHViewPager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        pager.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
