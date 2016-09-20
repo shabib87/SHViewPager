77a14664d1b154b6a247de502c62be3a16a0606b
shabib87 <shabib.sust@gmail.com>
2016-09-20 22:06:14 +0600

    Organize delegate and datasource methods enhancement

---
+++ SHViewPager/Classes/SHViewPagerDataSource.h
@@ -0,0 +1,13 @@
+//
+//  SHViewPagerDataSource.h
+//  Pods
+//
+//  Created by shabib hossain on 9/20/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SHViewPagerDataSource <NSObject>
+
+@end
---
+++ SHViewPager/Classes/SHViewPagerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SHViewPagerDelegate.h
+//  Pods
+//
+//  Created by shabib hossain on 9/20/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SHViewPagerDelegate <NSObject>
+
+@end
